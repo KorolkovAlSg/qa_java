3dce4cc9f977aba2d2067e063e66187d20980167
Korolkov Aleksey <korolkov.al.sg@yandex.ru>
2025-11-14 10:01:57 +0500

    Добавил мок в CatTest

--- src/test/java/CatTest.java
+++ src/test/java/CatTest.java
@@ -3,12 +3,20 @@ import com.example.Feline;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.List;
 
+@RunWith(MockitoJUnitRunner.class)
 public class CatTest {
 
     private Cat cat;
+
+    @Mock
+    private Feline felineMock;
 
     @Before
     public void setVar(){
@@ -24,6 +32,13 @@ public class CatTest {
     @Test
     public void getFoodInvokeReturnList() throws Exception {
         Assert.assertEquals("Метод должен вернуть список - \"Животные\", \"Птицы\", \"Рыба\"", List.of("Животные", "Птицы", "Рыба"), cat.getFood());
+    }
+
+    @Test
+    public void otherTestGetFoodWithMockReturnList() throws Exception {
+        cat = new Cat(felineMock);
+        Mockito.when(felineMock.eatMeat()).thenReturn(List.of("Трава", "Различные растения"));
+        Assert.assertEquals("Метод должен вернуть список - \"Трава\", \"Различные растения\"", List.of("Трава", "Различные растения"), cat.getFood());
     }
 
 }
